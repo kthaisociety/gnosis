a803912095d082d4cf1aaae521589af7faabec8e
novrion <elindstenz@outlook.com>
2025-11-23 14:37:11 +0100

    remove commented out vllm functionality

--- app/gateway/app/services/inference/vlm/__init__.py
+++ app/gateway/app/services/inference/vlm/__init__.py
@@ -1,6 +1,5 @@
 from .vlm import VLM
 from .transformer import VLMTransformer
 from .gemini import VLMGemini
-#from .vllm import VLMVLLM
 
-__all__ = ["VLM", "VLMTransformer", "VLMGemini"]#, "VLMVLLM"]
+__all__ = ["VLM", "VLMTransformer", "VLMGemini"]
